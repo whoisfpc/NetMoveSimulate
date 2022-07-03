711243f53b08966b5d55ee17c7d8538d0c1e50ac
whoisfpc <wo_fpc@outlook.com>
2022-07-03 20:32:21 +0800

    basic jump

--- Assets/Scripts/NetPlayer.cs
+++ Assets/Scripts/NetPlayer.cs
@@ -52,16 +52,28 @@ namespace NetMoveSimulate
 			}
 		}
 
+		private const float CHECK_GROUND_UP_OFFSET = 0.1f;
+		private const float GROUND_FLOAT_MAX_DIST = 0.023f;
+		private const float GROUND_FLOAT_MIN_DIST = 0.015f;
 		private const float AVOID_COLLIDER_DIST = 0.01f;
 		private const int MAX_RESOLVE_OVERLAP_COUNT = 3;
 
+		[Header("MoveSettings")]
 		public float maxSpeed = 5f;
+		public float groundAcceleration = 10f;
+		public float groundDeceleration = 20f;
+		public float airAcceleration = 5f;
+		public float airDeceleration = 0f;
 		public float rotateAngleSpeed = 360f;
+		public LayerMask groundMask;
+		public float maxStairHeight = 0.3f;
+		public float minGroundNormal = 0.707f;
+		public float jumpInitSpeed = 20f;
+
+		[Header("PlayerInfos")]
 		public Renderer bodyRenderer;
 		public uint id;
 		public CapsuleCollider capsule;
-		public LayerMask groundMask;
-		public float maxStairHeight = 0.3f;
 
 		private Vector2 moveInput;
 		private bool jumpInput;
@@ -71,12 +83,14 @@ namespace NetMoveSimulate
 		private uint sequence;
 
 		private readonly Collider[] penetrateOverlapCache = new Collider[64];
-		private Vector3 gravity = new Vector3(0, -0.98f, 0);
+		private Vector3 gravity = new Vector3(0, -9.8f, 0);
 		private Vector3 currentVelocity;
 		private MoveMode currentMoveMode = MoveMode.Walking;
 		private bool overlapResolved;
 		private CapsuleParamsCache capsuleCache;
 		private Vector3 groundNormal = Vector3.up;
+		// update every frame by CachePlayerMoveVector
+		private Vector3 playerMoveVector;
 
 		private void Awake()
 		{
@@ -109,7 +123,7 @@ namespace NetMoveSimulate
 
 		public void SetMoveInput(Vector2 moveInput)
 		{
-			this.moveInput = moveInput;
+			this.moveInput = moveInput.normalized;
 		}
 
 		public void Jump()
@@ -117,20 +131,33 @@ namespace NetMoveSimulate
 			jumpInput = true;
 		}
 
+		private void CachePlayerMoveVector()
+		{
+			Vector3 rightAxis = Vector3.ProjectOnPlane(inputSpace.right, Vector3.up);
+			Vector3 forwardAxis = Vector3.ProjectOnPlane(inputSpace.forward, Vector3.up);
+			playerMoveVector = rightAxis * moveInput.x + forwardAxis * moveInput.y;
+		}
+
 		private void Update()
 		{
 			if (isLocalPlayer)
 			{
+				float dt = Time.deltaTime;
+
+				CachePlayerMoveVector();
 				capsuleCache.RefreshShapeParams();
-				// check and do jump
 				// step 1: resolve penetrate
 				ResolvePenetrate();
+				// check and do jump
+				CheckAndTryJump();
 				//if (overlapResolved)
 				{
 					// step 2: check ground, change move mode
-					PerformMove();
+					PerformMove(dt);
 				}
+				PerformRotate(dt);
 				ClearJumpInput();
+				SendMoveMsg();
 			}
 		}
 
@@ -173,22 +200,31 @@ namespace NetMoveSimulate
 			transform.position += totalSeparateOffset;
 		}
 
-		private void PerformMove()
+		private void PerformMove(float dt)
 		{
 			switch (currentMoveMode)
 			{
 				case MoveMode.Walking:
-					PerformWalking();
+					PerformWalking(dt);
 					break;
 				case MoveMode.Falling:
-					PerformFalling();
+					PerformFalling(dt);
 					break;
 				default:
 					break;
 			}
 		}
 
-		private void PerformWalking()
+		private void PerformRotate(float dt)
+		{
+			if (playerMoveVector != Vector3.zero)
+			{
+				Quaternion targetRotation = Quaternion.LookRotation(playerMoveVector);
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, dt * rotateAngleSpeed);
+			}
+		}
+
+		private void PerformWalking(float dt)
 		{
 			// TODO: split delta time for
 			// TODO: acceleration and brake deceleration
@@ -197,16 +233,18 @@ namespace NetMoveSimulate
 			// if not on ground, switch to falling
 			// should have near ground falling?
 
-			Vector3 rightAxis = Vector3.ProjectOnPlane(inputSpace.right, Vector3.up);
-			Vector3 forwardAxis = Vector3.ProjectOnPlane(inputSpace.forward, Vector3.up);
-			Vector3 playerMoveVector = rightAxis * moveInput.x + forwardAxis * moveInput.y;
-
-			currentVelocity = maxSpeed * playerMoveVector;
-
-			if (playerMoveVector != Vector3.zero)
+			if (playerMoveVector == Vector3.zero)
 			{
-				Quaternion targetRotation = Quaternion.LookRotation(playerMoveVector);
-				transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * rotateAngleSpeed);
+				currentVelocity = Vector3.MoveTowards(currentVelocity, Vector3.zero, groundDeceleration * dt);
+			}
+			else
+			{
+				float effectiveAcceleration = groundAcceleration;
+				if (groundDeceleration > groundAcceleration && Vector3.Dot(playerMoveVector, currentVelocity) <= 0)
+				{
+					effectiveAcceleration = groundDeceleration;
+				}
+				currentVelocity = Vector3.ClampMagnitude(currentVelocity + dt * effectiveAcceleration * playerMoveVector, maxSpeed);
 			}
 
 			var currentHorizonVelocity = new Vector3(currentVelocity.x, 0, currentVelocity.z);
@@ -214,18 +252,36 @@ namespace NetMoveSimulate
 			var rampVelocity = new Vector3(currentVelocity.x,  -floorDotVel / groundNormal.y, currentVelocity.z);
 
 			currentVelocity = rampVelocity.normalized * currentVelocity.magnitude;
-			Vector3 canMoveDelta = SafeMove(currentVelocity * Time.deltaTime);
+			Vector3 canMoveDelta = SafeMove(currentVelocity * dt);
 			transform.position += canMoveDelta;
 			UpdateGround();
-
-			SendMoveMsg();
 		}
 
-		private void PerformFalling()
+		private void PerformFalling(float dt)
 		{
 			// add input acceleration and gravity
 			// sweep and slide, move and check ground (with additive check distance)
 			// if on ground, switch to walking
+			var currentHorizonVelocity = new Vector3(currentVelocity.x, 0, currentVelocity.z);
+			var verticalSpeed = currentVelocity.y;
+			if (playerMoveVector == Vector3.zero)
+			{
+				currentHorizonVelocity = Vector3.MoveTowards(currentHorizonVelocity, Vector3.zero, airDeceleration * dt);
+			}
+			else
+			{
+				float effectiveAcceleration = airAcceleration;
+				if (airDeceleration > airAcceleration && Vector3.Dot(playerMoveVector, currentHorizonVelocity) <= 0)
+				{
+					effectiveAcceleration = airDeceleration;
+				}
+				currentHorizonVelocity = Vector3.ClampMagnitude(currentHorizonVelocity + dt * effectiveAcceleration * playerMoveVector, maxSpeed);
+			}
+			verticalSpeed += gravity.y * dt;
+			currentVelocity = currentHorizonVelocity + Vector3.up * verticalSpeed;
+			Vector3 canMoveDelta = SafeMove(currentVelocity * dt);
+			transform.position += canMoveDelta;
+			UpdateGround();
 		}
 
 		private Vector3 SafeMove(Vector3 delta)
@@ -266,55 +322,49 @@ namespace NetMoveSimulate
 
 		private void UpdateGround()
 		{
-			Vector3 p1 = capsuleCache.PointP1;
-			Vector3 p2 = capsuleCache.PointP2;
+			Vector3 upOffset = Vector3.up * CHECK_GROUND_UP_OFFSET;
+			Vector3 p1 = capsuleCache.PointP1 + upOffset;
+			Vector3 p2 = capsuleCache.PointP2 + upOffset;
 			float radius = capsuleCache.Radius;
-			if (Physics.CapsuleCast(p1, p2, radius, Vector3.down, out RaycastHit hit, maxStairHeight, groundMask, QueryTriggerInteraction.Ignore))
+			float checkDist = maxStairHeight + CHECK_GROUND_UP_OFFSET + GROUND_FLOAT_MAX_DIST;
+			bool foundGround = false;
+			if (Physics.CapsuleCast(p1, p2, radius, Vector3.down, out RaycastHit hit, checkDist, groundMask, QueryTriggerInteraction.Ignore))
 			{
-				groundNormal = hit.normal;
+				foundGround = IsValidGroundHit(hit);
+			}
+
+			if (!foundGround)
+			{
+				SwitchMoveMode(MoveMode.Falling);
+			}
+			else if (currentVelocity.y < 0)
+			{
+				SwitchMoveMode(MoveMode.Walking);
 			}
 		}
 
-		private void LocalPlayerUpdate()
+		private bool IsValidGroundHit(RaycastHit hit)
 		{
-			Vector3 rightAxis = Vector3.ProjectOnPlane(inputSpace.right, Vector3.up);
-			Vector3 forwardAxis = Vector3.ProjectOnPlane(inputSpace.forward, Vector3.up);
-			Vector3 playerMoveVector = rightAxis * moveInput.x + forwardAxis * moveInput.y;
+			return hit.normal.y >= minGroundNormal;
+		}
 
-			currentVelocity = maxSpeed * playerMoveVector;
-
-			NetPosition += currentVelocity * Time.deltaTime;
-
-			if (playerMoveVector != Vector3.zero)
+		private void CheckAndTryJump()
+		{
+			if (jumpInput && currentMoveMode == MoveMode.Walking)
 			{
-				Quaternion targetRotation = Quaternion.LookRotation(playerMoveVector);
-				NetRotation = Quaternion.RotateTowards(NetRotation, targetRotation, Time.deltaTime * rotateAngleSpeed);
+				currentVelocity.y = Mathf.Max(currentVelocity.y + jumpInitSpeed, jumpInitSpeed);
+				SwitchMoveMode(MoveMode.Falling);
 			}
-
-			float checkGroundOffset = 1f;
-			Vector3 p1 = capsuleCache.PointP1 + Vector3.up * checkGroundOffset;
-			Vector3 p2 = capsuleCache.PointP2 + Vector3.up * checkGroundOffset;
-			float radius = capsuleCache.Radius;
-			float groundDistance = float.MaxValue;
-			if (Physics.CapsuleCast(p1, p2, radius, Vector3.down, out RaycastHit hit, 5f, groundMask, QueryTriggerInteraction.Ignore))
-			{
-				groundDistance = hit.distance - checkGroundOffset;
-			}
-
-			if (groundDistance > 0.01f)
-			{
-				float maxVerticalMove = Mathf.Min(5f * Time.deltaTime, groundDistance);
-				NetPosition += Vector3.down * maxVerticalMove;
-			}
-
-
-
-			SendMoveMsg();
 		}
 
 		private void ClearJumpInput()
 		{
 			jumpInput = false;
+		}
+
+		private void SwitchMoveMode(MoveMode newMode)
+		{
+			currentMoveMode = newMode;
 		}
 
 		private void SendMoveMsg()
